80e623386afb05026aa0ba0eb480b2f911b2abc4
Tyr Johanson <tyr@ca.com>
2017-09-21 09:40:58 -0600

    jasmine unit testing

---
+++ src/index.test.js
@@ -0,0 +1,15 @@
+describe('Example test set', () => {
+  it('should have written tests', () => {
+    expect(false).toBe(false);
+    // TODO (tj) Ext as module?
+    // expect(Ext.Date.format(new Date(), 'Y')).toEqual('2017');
+  });
+
+  // TODO (tj) Rally as module?
+  /*
+  it('should render the app', () => {
+    const app = Rally.test.Harness.launchApp('TSApp');
+    expect(app.getEl()).toBeDefined();
+  });
+  */
+});

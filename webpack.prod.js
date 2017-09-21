6fb050055c524c69cc79b1963ec79862bd3db2a5
Tyr Johanson <tyr@ca.com>
2017-09-20 19:53:41 -0600

    minified build output

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       inlineSource: '.(js|css)$', // embed all javascript and css inline
@@ -14,5 +15,11 @@ module.exports = merge(common, {
       template: 'src/index.ejs',
     }),
     new HtmlWebpackInlineSourcePlugin(),
+    new UglifyJSPlugin({
+      parallel: {
+        cache: true,
+        workers: 2,
+      },
+    }),
   ],
 });
